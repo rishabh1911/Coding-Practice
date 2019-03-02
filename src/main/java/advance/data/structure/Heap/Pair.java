11a0d29d87cf7cc7a31262f602b497fada1a01ec
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2019-03-02 22:16:34 +0530

    pair class

---
+++ src/main/java/advance/data/structure/Heap/Pair.java
@@ -0,0 +1,13 @@
+package advance.data.structure.Heap;
+
+public class Pair {
+	
+	int index1;
+    int index2;
+    
+    Pair(int index1, int index2) {
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+}
